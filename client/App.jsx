9b346d053f9744f21a7c06145d112564cfe0b5db
lyctea <396136337@qq.com>
2018-08-28 14:35:20 +0800

    <A> 开发环境的服务端渲染

--- client/App.jsx
+++ client/App.jsx
@@ -4,7 +4,8 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>This is App！</h1>
+        <h1>This is App!</h1>
+        <h2>热更新&&服务端渲染</h2>
       </div>
     )
   }
---
+++ server/util/dev-static.js
@@ -0,0 +1,72 @@
+const axios = require('axios')
+const webpack = require('webpack')
+const MemoryFs = require('memory-fs')
+const proxy = require('http-proxy-middleware')
+const path = require('path')
+const ReactDomServer = require('react-dom/server')
+
+const serverConfig = require('../../build/webpack.config.server')
+
+const getTemplate = () => {
+  return new Promise((resolve, reject) => {
+    axios.get('http://localhost:8888/public/index.html')
+      .then(res => {
+        resolve(res.data)
+      })
+      .catch(reject)
+  })
+}
+
+const Module = module.constructor
+
+const mfs = new MemoryFs()
+const serverCompiler = webpack(serverConfig)
+
+// 指定 compiler 输出
+serverCompiler.outputFileSystem= mfs
+
+let serverBundle
+serverCompiler.watch({}, (err, stats) => {
+  if(err) throw err
+  
+  stats = stats.toJson()
+  stats.errors.forEach(err => console.log(err))
+  stats.warnings.forEach(err => console.log(warn))
+  
+  // 获取服务端打包出来的文件路径
+  const bundlePath = path.join(
+    serverConfig.output.path,
+    serverConfig.output.filename
+  )
+  
+  const bundle = mfs.readFileSync(bundlePath, 'utf-8')
+  const m = new Module()
+  
+  m._compile(bundle, 'server-entry.js')
+  serverBundle = m.exports.default
+})
+
+module.exports = function (app) {
+  // 将 public 目录下的静态文件代理到 8888 端口
+  app.use('/public', proxy({
+    target: 'http://localhost:8888'
+  }))
+  
+  app.get('*', function (req, res) {
+    getTemplate().then(template => {
+      const content = ReactDomServer.renderToString(serverBundle)
+      res.send(template.replace('<!-- app -->', content))
+    })
+  })
+}
+
+
+/*
+* 开发环境服务端渲染的实现原理：
+* 监听wepack打包的结果，
+* 拿到这个结果，输出到 mfs 内存中，
+* 通过 hack 方法将获取的内容转成模块
+* 通过 ReactDomServer 方法得到内容
+* 替换 <!-- app --> 占位符
+*
+* */
